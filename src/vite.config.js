40b653d5fa4efe6b1557483804f34c6733c34535
elsa <elsaputri2038@gmail.com>
2024-05-21 18:32:42 +0700

    first commit

--- src/vite.config.js
+++ src/vite.config.js
@@ -5,16 +5,19 @@ import vue from '@vitejs/plugin-vue'
 import { quasar, transformAssetUrls } from '@quasar/vite-plugin'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue({
-      template: { transformAssetUrls }
-    }),
+export default {
+    build: {
+      rollupOptions: {
+        external: ['quasar']
+      }
+    }
+  }
 
     // @quasar/plugin-vite options list:
     // https://github.com/quasarframework/quasar/blob/dev/vite-plugin/index.d.ts
     quasar({
       sassVariables: 'src/quasar-variables.sass'
+
+
     })
-  ]
-})
+
